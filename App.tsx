406392a9ec3b4d6198654845b15b010965913f3b
asim9895 <asimjaipuri96@gmail.com>
2023-03-04 21:51:47 +0530

    welcome screen design

--- App.tsx
+++ App.tsx
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { appStyles } from "./app.style";
+import { useFonts, Nunito_600SemiBold } from "@expo-google-fonts/nunito";
+import * as Animatable from "react-native-animatable";
 
-export default function App() {
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+  });
+
+  if (!fontsLoaded) return null;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={appStyles.baseContainer}>
+      <ImageBackground
+        source={require("./assets/get_started.png")}
+        imageStyle={{
+          width: "100%",
+          height: 400,
+          opacity: 0.1,
+          borderBottomRightRadius: 300,
+        }}
+      >
+        <Animatable.Text
+          style={{
+            color: "#fff",
+            fontSize: 22,
+            marginTop: 350,
+            textAlign: "center",
+            fontFamily: "Nunito_600SemiBold",
+          }}
+          animation="slideInDown"
+        >
+          Welcome to BookStore
+        </Animatable.Text>
+        <Animatable.Text
+          style={{
+            color: "grey",
+            fontSize: 16,
+            marginTop: 20,
+            textAlign: "center",
+            fontFamily: "Nunito_600SemiBold",
+          }}
+          animation="slideInUp"
+        >
+          The number one best e-book store and reader application of the century
+        </Animatable.Text>
+      </ImageBackground>
+
+      <Animatable.View animation={"fadeInUp"}>
+        <TouchableOpacity>
+          <Text
+            style={{
+              color: "#fff",
+              fontSize: 16,
+              marginTop: 20,
+              textAlign: "center",
+              fontFamily: "Nunito_600SemiBold",
+              backgroundColor: "#526ee7",
+              marginHorizontal: 20,
+              borderRadius: 10,
+              padding: 7,
+            }}
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </Animatable.View>
+      <Animatable.View animation={"fadeInUp"}>
+        <TouchableOpacity>
+          <Text
+            style={{
+              color: "#fff",
+              fontSize: 16,
+              marginTop: 20,
+              textAlign: "center",
+              fontFamily: "Nunito_600SemiBold",
+              backgroundColor: "#171B29",
+              marginHorizontal: 20,
+              borderRadius: 10,
+              padding: 7,
+            }}
+          >
+            Already Have An Account
+          </Text>
+        </TouchableOpacity>
+      </Animatable.View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ app.style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const appStyles = StyleSheet.create({
+  baseContainer: {
+    flex: 1,
+    backgroundColor: "#131824",
+  },
+});
